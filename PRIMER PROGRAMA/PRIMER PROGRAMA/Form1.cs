db20aec9a52c7ca3f7195121fb41b8ee4510964d
Leandro <leabertero@hotmail.com>
2021-11-16 20:33:19 -0300

    primer programa

---
+++ PRIMER PROGRAMA/PRIMER PROGRAMA/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PRIMER_PROGRAMA
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnEjecutar_Click(object sender, EventArgs e)
+        {
+            LblMensaje.Text = "Hola clase";
+        }
+    }
+}
