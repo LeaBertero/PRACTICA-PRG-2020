5ae842fb1e65062dc6b83d54bd743c225b1c6f8b
Leandro <leabertero@hotmail.com>
2021-11-29 18:25:15 -0300

    prueba

--- prueba 1/prueba 1/Prueba 1.Designer.cs
+++ prueba 1/prueba 1/Prueba 1.Designer.cs
@@ -34,7 +34,7 @@ namespace prueba_1
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(102, 74);
+            this.button1.Location = new System.Drawing.Point(288, 63);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(159, 23);
             this.button1.TabIndex = 0;
--- prueba 1/prueba 1/Prueba 1.cs
+++ prueba 1/prueba 1/Prueba 1.cs
@@ -22,5 +22,7 @@ namespace prueba_1
             Form2 formulario = new Form2();
             formulario.Show();
         }
+
+       
     }
 }
