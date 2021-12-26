e292c8da06966e48e4df3adb8ee378a06fd244b1
Leandro Bertero <leabertero@hotmail.com>
2021-12-26 14:01:57 -0300

    se añadio un icono al formulario

--- Ejercicio_Operadores/Ejercicio_Operadores/Calculadora.Designer.cs
+++ Ejercicio_Operadores/Ejercicio_Operadores/Calculadora.Designer.cs
@@ -29,6 +29,7 @@ namespace Ejercicio_Operadores
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Calculadora));
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.TxtSum1 = new System.Windows.Forms.TextBox();
@@ -374,6 +375,7 @@ namespace Ejercicio_Operadores
             this.Controls.Add(this.TxtSum1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Calculadora";
             this.Text = "Calculadora";
             this.ResumeLayout(false);
