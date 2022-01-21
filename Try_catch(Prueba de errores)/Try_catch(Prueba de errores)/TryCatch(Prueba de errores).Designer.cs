81a291c61b80d03763d470af44c2714149e3c174
Leandro Bertero <leabertero@hotmail.com>
2022-01-21 15:24:51 -0300

    cambio de tamaño del formulario

--- Try_catch(Prueba de errores)/Try_catch(Prueba de errores)/TryCatch(Prueba de errores).Designer.cs
+++ Try_catch(Prueba de errores)/Try_catch(Prueba de errores)/TryCatch(Prueba de errores).Designer.cs
@@ -145,7 +145,7 @@ namespace Try_catch_Prueba_de_errores_
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ControlDark;
-            this.ClientSize = new System.Drawing.Size(800, 181);
+            this.ClientSize = new System.Drawing.Size(664, 181);
             this.Controls.Add(this.BtnCleanError);
             this.Controls.Add(this.BtnCleanRes);
             this.Controls.Add(this.Btnclean);
