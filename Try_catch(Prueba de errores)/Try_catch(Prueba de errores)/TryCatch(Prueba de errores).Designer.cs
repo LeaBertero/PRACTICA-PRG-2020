3e7d7186f51bab5a029226f120fedda47fd85855
Leandro Bertero <leabertero@hotmail.com>
2022-01-22 21:22:21 -0300

    se agrego la operacion de la resta

--- Try_catch(Prueba de errores)/Try_catch(Prueba de errores)/TryCatch(Prueba de errores).Designer.cs
+++ Try_catch(Prueba de errores)/Try_catch(Prueba de errores)/TryCatch(Prueba de errores).Designer.cs
@@ -30,16 +30,21 @@ namespace Try_catch_Prueba_de_errores_
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
-            this.TxtNum1 = new System.Windows.Forms.TextBox();
-            this.TxtNum2 = new System.Windows.Forms.TextBox();
-            this.BtnRes = new System.Windows.Forms.Button();
+            this.TxtSum1 = new System.Windows.Forms.TextBox();
+            this.TxtSum2 = new System.Windows.Forms.TextBox();
+            this.BtnSuma = new System.Windows.Forms.Button();
             this.LblError = new System.Windows.Forms.Label();
-            this.Lblres = new System.Windows.Forms.Label();
+            this.LblresSuma = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
             this.Btnclean = new System.Windows.Forms.Button();
             this.BtnCleanRes = new System.Windows.Forms.Button();
             this.BtnCleanError = new System.Windows.Forms.Button();
             this.BtnSalir = new System.Windows.Forms.Button();
+            this.LblResResta = new System.Windows.Forms.Label();
+            this.BtnResta = new System.Windows.Forms.Button();
+            this.TxtRes2 = new System.Windows.Forms.TextBox();
+            this.TxtRes1 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
@@ -56,49 +61,48 @@ namespace Try_catch_Prueba_de_errores_
             this.button1.Text = "+";
             this.button1.UseVisualStyleBackColor = true;
             // 
-            // TxtNum1
+            // TxtSum1
             // 
-            this.TxtNum1.Location = new System.Drawing.Point(153, 55);
-            this.TxtNum1.Name = "TxtNum1";
-            this.TxtNum1.Size = new System.Drawing.Size(100, 20);
-            this.TxtNum1.TabIndex = 1;
-            this.TxtNum1.KeyUp += new System.Windows.Forms.KeyEventHandler(this.TxtNum1_KeyUp);
+            this.TxtSum1.Location = new System.Drawing.Point(153, 55);
+            this.TxtSum1.Name = "TxtSum1";
+            this.TxtSum1.Size = new System.Drawing.Size(100, 20);
+            this.TxtSum1.TabIndex = 1;
             // 
-            // TxtNum2
+            // TxtSum2
             // 
-            this.TxtNum2.Location = new System.Drawing.Point(361, 55);
-            this.TxtNum2.Name = "TxtNum2";
-            this.TxtNum2.Size = new System.Drawing.Size(100, 20);
-            this.TxtNum2.TabIndex = 2;
+            this.TxtSum2.Location = new System.Drawing.Point(361, 55);
+            this.TxtSum2.Name = "TxtSum2";
+            this.TxtSum2.Size = new System.Drawing.Size(100, 20);
+            this.TxtSum2.TabIndex = 2;
             // 
-            // BtnRes
+            // BtnSuma
             // 
-            this.BtnRes.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.BtnRes.Location = new System.Drawing.Point(497, 51);
-            this.BtnRes.Name = "BtnRes";
-            this.BtnRes.Size = new System.Drawing.Size(82, 27);
-            this.BtnRes.TabIndex = 3;
-            this.BtnRes.Text = "=";
-            this.BtnRes.UseVisualStyleBackColor = true;
-            this.BtnRes.Click += new System.EventHandler(this.BtnRes_Click);
+            this.BtnSuma.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnSuma.Location = new System.Drawing.Point(497, 51);
+            this.BtnSuma.Name = "BtnSuma";
+            this.BtnSuma.Size = new System.Drawing.Size(82, 27);
+            this.BtnSuma.TabIndex = 3;
+            this.BtnSuma.Text = "=";
+            this.BtnSuma.UseVisualStyleBackColor = true;
+            this.BtnSuma.Click += new System.EventHandler(this.BtnSuma_Click_1);
             // 
             // LblError
             // 
             this.LblError.AutoSize = true;
             this.LblError.Font = new System.Drawing.Font("Palatino Linotype", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.LblError.Location = new System.Drawing.Point(21, 150);
+            this.LblError.Location = new System.Drawing.Point(26, 432);
             this.LblError.Name = "LblError";
             this.LblError.Size = new System.Drawing.Size(0, 22);
             this.LblError.TabIndex = 4;
             // 
-            // Lblres
+            // LblresSuma
             // 
-            this.Lblres.AutoSize = true;
-            this.Lblres.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Lblres.Location = new System.Drawing.Point(618, 58);
-            this.Lblres.Name = "Lblres";
-            this.Lblres.Size = new System.Drawing.Size(0, 20);
-            this.Lblres.TabIndex = 5;
+            this.LblresSuma.AutoSize = true;
+            this.LblresSuma.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblresSuma.Location = new System.Drawing.Point(618, 58);
+            this.LblresSuma.Name = "LblresSuma";
+            this.LblresSuma.Size = new System.Drawing.Size(0, 20);
+            this.LblresSuma.TabIndex = 5;
             // 
             // label1
             // 
@@ -113,7 +117,7 @@ namespace Try_catch_Prueba_de_errores_
             // Btnclean
             // 
             this.Btnclean.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Btnclean.Location = new System.Drawing.Point(153, 97);
+            this.Btnclean.Location = new System.Drawing.Point(153, 366);
             this.Btnclean.Name = "Btnclean";
             this.Btnclean.Size = new System.Drawing.Size(107, 45);
             this.Btnclean.TabIndex = 7;
@@ -124,7 +128,7 @@ namespace Try_catch_Prueba_de_errores_
             // BtnCleanRes
             // 
             this.BtnCleanRes.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.BtnCleanRes.Location = new System.Drawing.Point(266, 97);
+            this.BtnCleanRes.Location = new System.Drawing.Point(266, 366);
             this.BtnCleanRes.Name = "BtnCleanRes";
             this.BtnCleanRes.Size = new System.Drawing.Size(107, 45);
             this.BtnCleanRes.TabIndex = 8;
@@ -135,7 +139,7 @@ namespace Try_catch_Prueba_de_errores_
             // BtnCleanError
             // 
             this.BtnCleanError.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.BtnCleanError.Location = new System.Drawing.Point(379, 97);
+            this.BtnCleanError.Location = new System.Drawing.Point(379, 366);
             this.BtnCleanError.Name = "BtnCleanError";
             this.BtnCleanError.Size = new System.Drawing.Size(107, 45);
             this.BtnCleanError.TabIndex = 9;
@@ -146,31 +150,84 @@ namespace Try_catch_Prueba_de_errores_
             // BtnSalir
             // 
             this.BtnSalir.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.BtnSalir.Location = new System.Drawing.Point(566, 181);
+            this.BtnSalir.Location = new System.Drawing.Point(610, 364);
             this.BtnSalir.Name = "BtnSalir";
-            this.BtnSalir.Size = new System.Drawing.Size(90, 36);
+            this.BtnSalir.Size = new System.Drawing.Size(94, 121);
             this.BtnSalir.TabIndex = 10;
             this.BtnSalir.Text = "Salir";
             this.BtnSalir.UseVisualStyleBackColor = true;
             this.BtnSalir.Click += new System.EventHandler(this.BtnSalir_Click);
             // 
+            // LblResResta
+            // 
+            this.LblResResta.AutoSize = true;
+            this.LblResResta.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblResResta.Location = new System.Drawing.Point(618, 133);
+            this.LblResResta.Name = "LblResResta";
+            this.LblResResta.Size = new System.Drawing.Size(0, 20);
+            this.LblResResta.TabIndex = 15;
+            // 
+            // BtnResta
+            // 
+            this.BtnResta.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnResta.Location = new System.Drawing.Point(497, 126);
+            this.BtnResta.Name = "BtnResta";
+            this.BtnResta.Size = new System.Drawing.Size(82, 27);
+            this.BtnResta.TabIndex = 14;
+            this.BtnResta.Text = "=";
+            this.BtnResta.UseVisualStyleBackColor = true;
+            this.BtnResta.Click += new System.EventHandler(this.BtnResta_Click);
+            // 
+            // TxtRes2
+            // 
+            this.TxtRes2.Location = new System.Drawing.Point(361, 130);
+            this.TxtRes2.Name = "TxtRes2";
+            this.TxtRes2.Size = new System.Drawing.Size(100, 20);
+            this.TxtRes2.TabIndex = 13;
+            // 
+            // TxtRes1
+            // 
+            this.TxtRes1.Location = new System.Drawing.Point(153, 130);
+            this.TxtRes1.Name = "TxtRes1";
+            this.TxtRes1.Size = new System.Drawing.Size(100, 20);
+            this.TxtRes1.TabIndex = 12;
+            // 
+            // button3
+            // 
+            this.button3.Enabled = false;
+            this.button3.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.button3.FlatAppearance.BorderSize = 4;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(287, 122);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(38, 35);
+            this.button3.TabIndex = 11;
+            this.button3.Text = "-";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
             // TryCatch_Prueba_de_errores_
             // 
-            this.AcceptButton = this.BtnRes;
+            this.AcceptButton = this.BtnSuma;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.SystemColors.ControlDark;
-            this.ClientSize = new System.Drawing.Size(664, 225);
+            this.BackColor = System.Drawing.Color.YellowGreen;
+            this.ClientSize = new System.Drawing.Size(708, 489);
+            this.Controls.Add(this.LblResResta);
+            this.Controls.Add(this.BtnResta);
+            this.Controls.Add(this.TxtRes2);
+            this.Controls.Add(this.TxtRes1);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.BtnSalir);
             this.Controls.Add(this.BtnCleanError);
             this.Controls.Add(this.BtnCleanRes);
             this.Controls.Add(this.Btnclean);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.Lblres);
+            this.Controls.Add(this.LblresSuma);
             this.Controls.Add(this.LblError);
-            this.Controls.Add(this.BtnRes);
-            this.Controls.Add(this.TxtNum2);
-            this.Controls.Add(this.TxtNum1);
+            this.Controls.Add(this.BtnSuma);
+            this.Controls.Add(this.TxtSum2);
+            this.Controls.Add(this.TxtSum1);
             this.Controls.Add(this.button1);
             this.Cursor = System.Windows.Forms.Cursors.Hand;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
@@ -186,15 +243,20 @@ namespace Try_catch_Prueba_de_errores_
         #endregion
 
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.TextBox TxtNum1;
-        private System.Windows.Forms.TextBox TxtNum2;
-        private System.Windows.Forms.Button BtnRes;
+        private System.Windows.Forms.TextBox TxtSum1;
+        private System.Windows.Forms.TextBox TxtSum2;
+        private System.Windows.Forms.Button BtnSuma;
         private System.Windows.Forms.Label LblError;
-        private System.Windows.Forms.Label Lblres;
+        private System.Windows.Forms.Label LblresSuma;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button Btnclean;
         private System.Windows.Forms.Button BtnCleanRes;
         private System.Windows.Forms.Button BtnCleanError;
         private System.Windows.Forms.Button BtnSalir;
+        private System.Windows.Forms.Label LblResResta;
+        private System.Windows.Forms.Button BtnResta;
+        private System.Windows.Forms.TextBox TxtRes2;
+        private System.Windows.Forms.TextBox TxtRes1;
+        private System.Windows.Forms.Button button3;
     }
 }
--- Try_catch(Prueba de errores)/Try_catch(Prueba de errores)/TryCatch(Prueba de errores).cs
+++ Try_catch(Prueba de errores)/Try_catch(Prueba de errores)/TryCatch(Prueba de errores).cs
@@ -17,17 +17,18 @@ namespace Try_catch_Prueba_de_errores_
             InitializeComponent();
         }
 
-        private void BtnRes_Click(object sender, EventArgs e)
+       
+        private void BtnSuma_Click_1(object sender, EventArgs e)
         {
 
             try
             {
 
-                Lblres.Text = (Convert.ToInt32(TxtNum1.Text)
-                     + Convert.ToInt32(TxtNum2.Text)).ToString();
+                LblresSuma.Text = (Convert.ToInt32(TxtSum1.Text)
+                     + Convert.ToInt32(TxtSum2.Text)).ToString();
 
             }
-            catch (Exception )
+            catch (Exception)
             {
                 LblError.Text = "imposible sumar, debe agregar dos numeros";
                 //LblError.Text = Error.Message;
@@ -35,40 +36,63 @@ namespace Try_catch_Prueba_de_errores_
             }
 
         }
-           
+
+
+        private void BtnResta_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+                LblResResta.Text = (Convert.ToInt32(TxtRes1.Text)
+                     - Convert.ToInt32(TxtRes2.Text)).ToString();
+
+            }
+            catch (Exception)
+            {
+                LblError.Text = "imposible restar, debe agregar dos numeros";
+                //LblError.Text = Error.Message;
+                //MessageBox.Show("imposible sumar, debe agregar dos numeros");
+            }
+        }
+
 
 
         private void Btnclean_Click(object sender, EventArgs e)
         {
-            TxtNum1.Text = null;
-            TxtNum2.Text = null;
+            TxtSum1.Text = null;
+            TxtSum2.Text = null;
+            TxtRes1.Text = null;
+            TxtRes2.Text = null;
         }
 
         private void BtnCleanRes_Click(object sender, EventArgs e)
         {
-            Lblres.Text = null;
+            LblresSuma.Text = null;
+            LblResResta.Text = null;
         }
 
         private void BtnCleanError_Click(object sender, EventArgs e)
         {
             LblError.Text = null;
         }
+            
 
-        private void TxtNum1_KeyUp(object sender, KeyEventArgs e)
-        {
-            if (e.KeyCode == Keys.Enter)
-            {
+        //private void TxtNum1_KeyUp(object sender, KeyEventArgs e)
+        //{
+        //    if (e.KeyCode == Keys.Enter)
+        //    {
 
-                LblError.Text = "Imposible sumar letras";
+        //        LblError.Text = "Imposible sumar letras";
 
-            }
-        }
+        //    }
+        //}
 
         private void BtnSalir_Click(object sender, EventArgs e)
         {
             Close();
         }
 
+       
     }
         
 }
