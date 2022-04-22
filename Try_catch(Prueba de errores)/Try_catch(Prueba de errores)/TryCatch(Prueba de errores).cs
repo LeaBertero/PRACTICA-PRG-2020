2251e8616eff4d02f75e80399be49f2e5d01b48a
Leandro Bertero <leabertero@hotmail.com>
2022-04-22 01:30:59 -0300

    correccion en el programa, se limpiaron los textbox con null luego de ingresar numeros para sumar

--- Try_catch(Prueba de errores)/Try_catch(Prueba de errores)/TryCatch(Prueba de errores).cs
+++ Try_catch(Prueba de errores)/Try_catch(Prueba de errores)/TryCatch(Prueba de errores).cs
@@ -21,11 +21,15 @@ namespace Try_catch_Prueba_de_errores_
         private void BtnSuma_Click_1(object sender, EventArgs e)
         {
 
+            TxtSum1.Text = null;
+            TxtSum2.Text = null;
+
             try
             {
 
                 LblresSuma.Text = (Convert.ToInt32(TxtSum1.Text)
                      + Convert.ToInt32(TxtSum2.Text)).ToString();
+                
 
             }
             catch (Exception)
@@ -38,8 +42,14 @@ namespace Try_catch_Prueba_de_errores_
         }
 
 
+        
+
         private void BtnResta_Click(object sender, EventArgs e)
         {
+
+            TxtRes1.Text = null;
+            TxtRes2.Text = null;
+
             try
             {
 
