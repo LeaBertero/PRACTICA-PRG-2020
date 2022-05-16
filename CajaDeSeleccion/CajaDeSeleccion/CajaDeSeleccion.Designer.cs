75c915985182499461687af0395837f14bcb6d9c
Leandro Bertero <leabertero@hotmail.com>
2022-05-16 20:51:30 -0300

    se movio el boton hacia abajo a la derecha

--- CajaDeSeleccion/CajaDeSeleccion/CajaDeSeleccion.Designer.cs
+++ CajaDeSeleccion/CajaDeSeleccion/CajaDeSeleccion.Designer.cs
@@ -33,7 +33,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(398, 354);
+            this.button1.Location = new System.Drawing.Point(627, 361);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(103, 50);
             this.button1.TabIndex = 0;
