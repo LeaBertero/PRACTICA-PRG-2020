ba0b0f73cea12bc1afc985456368bd3e642b48c1
Leandro Bertero <leabertero@hotmail.com>
2022-05-17 23:39:15 -0300

    correccion en variables

--- GradosCelciusAFarenheitViceversa/GradosCelciusAFarenheitViceversa/Form1.cs
+++ GradosCelciusAFarenheitViceversa/GradosCelciusAFarenheitViceversa/Form1.cs
@@ -2,6 +2,10 @@ namespace GradosCelciusAFarenheitViceversa
 {
     public partial class Form1 : Form
     {
+
+        double gcentigrados, ct1;
+        double gfarenheit;
+
         public Form1()
         {
             InitializeComponent();
@@ -21,14 +25,14 @@ namespace GradosCelciusAFarenheitViceversa
 
         private void BtFar_Click(object sender, EventArgs e)
         {
-            double gcentigrados, ct1;
+            
             ct1 = Convert.ToDouble(TxTemp.Text);
             gcentigrados = (ct1 - 32.0) / 1.8;
             TxRes.Text = String.Format("{0:F3}", gcentigrados);
         }
         private void BtCelcius_Click(object sender, EventArgs e)
         {
-            double gfarenheit, ct1;
+            
             ct1 = Convert.ToDouble(TxTemp.Text);
             gfarenheit = ct1 * 1.8 + 32.0;
             TxRes.Text = String.Format("{0:F3}", gfarenheit);
