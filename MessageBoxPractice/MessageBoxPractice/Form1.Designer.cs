4f3aa5507b764a5494c98d5514c463ac53bf3156
Leandro Bertero <leabertero@hotmail.com>
2022-05-22 14:58:41 -0300

    fin programa - correccion de errores(Solucionados)

--- MessageBoxPractice/MessageBoxPractice/Form1.Designer.cs
+++ MessageBoxPractice/MessageBoxPractice/Form1.Designer.cs
@@ -236,7 +236,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(716, 437);
             this.Controls.Add(this.BtSalir);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
