2464a7452e12d77c8831cf66eff0255ddac9fedf
Leandro Bertero <leabertero@hotmail.com>
2022-05-27 12:56:14 -0300

    cambios en diseño

--- Ej_checkbob/Ej_checkbob/Prg_Checkbox.Designer.cs
+++ Ej_checkbob/Ej_checkbob/Prg_Checkbox.Designer.cs
@@ -74,7 +74,7 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Tempus Sans ITC", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(69, 278);
+            this.label1.Location = new System.Drawing.Point(11, 284);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(0, 20);
             this.label1.TabIndex = 3;
@@ -116,7 +116,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.OrangeRed;
-            this.ClientSize = new System.Drawing.Size(366, 329);
+            this.ClientSize = new System.Drawing.Size(385, 329);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.BtSalir);
             this.Controls.Add(this.Btlimpiar);
