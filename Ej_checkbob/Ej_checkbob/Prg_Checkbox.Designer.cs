b16afdeeb83297625f783afa5e66a802fb3d0aa7
Leandro Bertero <leabertero@hotmail.com>
2022-05-27 12:38:42 -0300

    Seleccion de opciones

---
+++ Ej_checkbob/Ej_checkbob/Prg_Checkbox.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace Ej_checkbob
+{
+    partial class Prg_Checkbox
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.BtAceptar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Btlimpiar = new System.Windows.Forms.Button();
+            this.BtSalir = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBox1.Location = new System.Drawing.Point(72, 100);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(91, 24);
+            this.checkBox1.TabIndex = 0;
+            this.checkBox1.Text = "Hombre";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBox2.Location = new System.Drawing.Point(72, 130);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(72, 24);
+            this.checkBox2.TabIndex = 1;
+            this.checkBox2.Text = "Mujer";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // BtAceptar
+            // 
+            this.BtAceptar.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtAceptar.Location = new System.Drawing.Point(72, 169);
+            this.BtAceptar.Name = "BtAceptar";
+            this.BtAceptar.Size = new System.Drawing.Size(75, 23);
+            this.BtAceptar.TabIndex = 2;
+            this.BtAceptar.Text = "Aceptar";
+            this.BtAceptar.UseVisualStyleBackColor = true;
+            this.BtAceptar.Click += new System.EventHandler(this.BtAceptar_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(69, 278);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 3;
+            // 
+            // Btlimpiar
+            // 
+            this.Btlimpiar.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Btlimpiar.Location = new System.Drawing.Point(153, 169);
+            this.Btlimpiar.Name = "Btlimpiar";
+            this.Btlimpiar.Size = new System.Drawing.Size(112, 23);
+            this.Btlimpiar.TabIndex = 4;
+            this.Btlimpiar.Text = "Limpiar controles";
+            this.Btlimpiar.UseVisualStyleBackColor = true;
+            this.Btlimpiar.Click += new System.EventHandler(this.Btlimpiar_Click);
+            // 
+            // BtSalir
+            // 
+            this.BtSalir.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtSalir.Location = new System.Drawing.Point(72, 209);
+            this.BtSalir.Name = "BtSalir";
+            this.BtSalir.Size = new System.Drawing.Size(212, 23);
+            this.BtSalir.TabIndex = 5;
+            this.BtSalir.Text = "Cerrar programa";
+            this.BtSalir.UseVisualStyleBackColor = true;
+            this.BtSalir.Click += new System.EventHandler(this.BtSalir_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Ink Free", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(39, 49);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(298, 26);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Seleccione una de las opciones";
+            // 
+            // Prg_Checkbox
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.OrangeRed;
+            this.ClientSize = new System.Drawing.Size(366, 353);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.BtSalir);
+            this.Controls.Add(this.Btlimpiar);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.BtAceptar);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Name = "Prg_Checkbox";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Check_box";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.Button BtAceptar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button Btlimpiar;
+        private System.Windows.Forms.Button BtSalir;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Ej_checkbob/Ej_checkbob/Prg_Checkbox.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ej_checkbob
+{
+    public partial class Prg_Checkbox : Form
+    {
+        public Prg_Checkbox()
+        {
+            InitializeComponent();
+        }
+
+        private void BtAceptar_Click(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked || checkBox2.Checked == false)
+            {
+                label1.Text = "Seleccione una de las opciones";
+            }
+            if (checkBox1.Checked == true)
+            {
+                label1.Text = "hombre - ingrese sus datos";
+            }
+            if (checkBox2.Checked == true)
+            {
+                label1.Text = "Mujer . Ingrese sus datos";
+            }
+            
+            
+            
+        }
+
+        private void Btlimpiar_Click(object sender, EventArgs e)
+        {
+            label1.Text = null;
+        }
+
+        private void BtSalir_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
+           
+            
+           
+
+               
+            
